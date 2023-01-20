4d41f83eaae2329d8ea3f32d23d84e98b18f472a
moa3337 <moa-x@hotmail.com>
2023-01-20 17:20:58 +0100

    final commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// nome
+let name = prompt('Come ti chiami?')
+
+// cognome
+let sur_name = prompt('Quale è il tuo cognome?')
+
+// colore
+let color = prompt('quale è il tuo colore preferito?')
+
+// richiamo all'h1 
+document.getElementById('welcome').innerHTML = name + sur_name + color + '23' 
+
+
+//console.log(name + sur_name + color + " 23 ")
