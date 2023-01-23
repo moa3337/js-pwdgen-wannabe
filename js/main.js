0ccd10163d02f683c7cceaa47901f29b9d14cea1
moa3337 <moa-x@hotmail.com>
2023-01-23 10:28:28 +0100

    correzione final commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // nome
-let name = prompt('Come ti chiami?')
+let user_name = prompt('Come ti chiami?')
 
 // cognome
-let sur_name = prompt('Quale è il tuo cognome?')
+let user_surname = prompt('Quale è il tuo cognome?')
 
 // colore
 let color = prompt('quale è il tuo colore preferito?')
 
+// concatenazione
+const userpasword = user_name + user_surname + color + "23"
+
 // richiamo all'h1 
-document.getElementById('welcome').innerHTML = name + sur_name + color + '23' 
+document.getElementById('welcome').innerHTML = userpasword
 
 
-//console.log(name + sur_name + color + " 23 ")
+console.log(userpasword)
